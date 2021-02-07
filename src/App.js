4bf700d7cbec592065441d2fb2114dd7b1d7a282
LapitskayaN <lapitskaya.nl@gmail.com>
2021-02-07 17:01:57 +0300

    added component ContactList

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@
 import './App.css';
 import React from 'react';
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-        <Contact />
+      <Contact
+        name="Ralph Wiggum"
+        image="https://randomuser.me/api/portraits/men/85.jpg"
+        status="online"
+       
+      />
+      <Contact
+        name="Peter Palmer"
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
         
+      />
+      <Contact
+        name="Yan Mihal"
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        status="online"
+        
+      />
+      <ContactList />
+
+
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import './Contact.css';
 
-function Contact() {
+function Contact(props) {
   return (
     <div className="Contact"> 
       <div className="avatar">
-          <img  className="avatar" src="https://randomuser.me/api/portraits/women/10.jpg"></img>  
+          <img  className="avatar" src={props.image}></img>  
           {/* /// <a href="https://randomuser.me/api/portraits/women/10.jpg">aaa</a>     */}
       </div>
       <div>        
-          <p className="name">Georgia Morris</p>        
+          <p className="name">{props.name}</p>        
           <div className="status">
-            <p className="status-text">
+          <div className={props.status ? "status-online" : "status-offline"} />
+          <p className="status-text">{props.status ? "online" : "offline"}</p>
+       
+            {/* <p className="status-text">
               <span className="status-online"></span>Online
-            </p>
+            </p> */}
           </div>
       </div>
 
@@ -21,5 +25,9 @@ function Contact() {
     
   );
 }
+Contact.propTypes = {
+  characterFirstName: PropTypes.string.isRequired,
+  characterLastName: PropTypes.string.isRequired
+};
 
 export default Contact;
